85a27cb923d48db0a7665f80211dd77258c2abb0
Pragati Gupta <50272471+PragatiiGupta@users.noreply.github.com>
2024-02-02 14:43:32 +0530

    Update Sql_04.sql

--- Sql_04.sql
+++ Sql_04.sql
@@ -1,11 +1,13 @@
 write a SQL query to find the customers who have placed the most orders.
   
+Answer 1  
 select c.customer_name, count(order_id) as qty from Orders o
 join Customers c on o.customer_id = c.customer_id
 group by o.customer_id
 order by qty desc
 limit 1;
-OR
+
+Answer 2
 select customer_name from (
 select c.customer_name, count(order_id) as qty,rank() over(order by count(order_id) desc) as rank
 from Orders o
