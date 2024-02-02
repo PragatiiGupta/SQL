10cacb00fb69ac0084192b36279c6dacd6665bdf
Pragati Gupta <50272471+PragatiiGupta@users.noreply.github.com>
2024-02-02 14:44:03 +0530

    Update Sql_05.sql

--- Sql_05.sql
+++ Sql_05.sql
@@ -1,10 +1,12 @@
 In a database with tables Students and Enrollments, write a query to find all students who have not enrolled in any courses.
-
+    
+Answer 1
 select s.student_id, student_name from 
 Students s left join Enrollments e on
 s.student_id = e.student_id
 where e.student_id is null;
-OR
+    
+Answer 2
 select student_id, student_name
 from Students
 where not exists (
